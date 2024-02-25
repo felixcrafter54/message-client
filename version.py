47347d0bf5e1bf8156361bc0addc5f5996a538ba
felixcrafter54 <felixcrafter54@gmail.com>
2024-02-25 23:12:34 +0100

    changed LICENCE to GPL-3.0

--- version.py
+++ version.py
@@ -2,9 +2,9 @@ import pyinstaller_versionfile
 
 pyinstaller_versionfile.create_versionfile(
     output_file="versionfile.txt",
-    version="1.5",
+    version="1.6",
     product_name="Message-Client",
     file_description="Message-Client",
-    legal_copyright="MIT License Copyright (c) 2024 Felix felixcrafter54@gmail.com",
+    legal_copyright="GPL-3.0 license Copyright (c) 2024 Felix felixcrafter54@gmail.com",
     company_name="Felix Noster"
     )
