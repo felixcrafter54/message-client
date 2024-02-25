e636e5294b06aea4ccbcfe38fb01db58d55a99f6
felixcrafter54 <felixcrafter54@gmail.com>
2024-02-25 21:44:32 +0100

    created new exe and updated year

--- version.py
+++ version.py
@@ -2,9 +2,9 @@ import pyinstaller_versionfile
 
 pyinstaller_versionfile.create_versionfile(
     output_file="versionfile.txt",
-    version="1.4",
+    version="1.5",
     product_name="Message-Client",
     file_description="Message-Client",
-    legal_copyright="MIT License Copyright (c) 2023 Felix felixcrafter54@gmail.com",
+    legal_copyright="MIT License Copyright (c) 2024 Felix felixcrafter54@gmail.com",
     company_name="Felix Noster"
     )
