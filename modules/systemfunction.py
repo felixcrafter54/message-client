efe33194c1f500cf0c38891894aa7f5358b4e1ef
Sterndu <57641750+SternduCode@users.noreply.github.com>
2024-02-25 20:24:41 +0000

    Delayed execution of systemfunctions so that the response gets sent before execution

--- modules/systemfunction.py
+++ modules/systemfunction.py
@@ -1,15 +1,19 @@
 import ctypes
 import os
+import threading
 
 def execute_systemfunction(function):
     if function == "standby":
-        ctypes.windll.powrprof.SetSuspendState(False, False, False)
+        run_thread_delayed(lambda: ctypes.windll.powrprof.SetSuspendState(False, False, False))
     elif function == "lock":
-        ctypes.windll.user32.LockWorkStation()
+        run_thread_delayed(lambda: ctypes.windll.user32.LockWorkStation())
     elif function == "shutdown":
-        os.system("shutdown /s /t 0")
+        run_thread_delayed(lambda: os.system("shutdown /s /t 0"))
     elif function == "reboot":
-        os.system("shutdown /r /t 0")
+        run_thread_delayed(lambda: os.system("shutdown /r /t 0"))
     else:
         return "Unknown Systemfunction: " + function
-    return "Systemfunction executed"
+    return "Systemfunction executing"
+
+def run_thread_delayed(task, delay = 1):
+    threading.Timer(delay, task).start()
