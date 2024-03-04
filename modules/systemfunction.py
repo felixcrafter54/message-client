beaf493ae62b3b6db19c6164933ab6435d0356f3
felixcrafter54 <felixcrafter54@gmail.com>
2024-03-04 21:30:14 +0100

    Added screen_on and screen_off to systemfunction.py

--- modules/systemfunction.py
+++ modules/systemfunction.py
@@ -1,6 +1,14 @@
 import ctypes
 import os
 import threading
+import win32api, win32con
+
+def move_cursor():
+    x, y = (0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y)
+
+def screen_on():
+    move_cursor()
 
 def execute_systemfunction(function):
     if function == "standby":
@@ -11,6 +19,11 @@ def execute_systemfunction(function):
         run_thread_delayed(lambda: os.system("shutdown /s /t 0"))
     elif function == "reboot":
         run_thread_delayed(lambda: os.system("shutdown /r /t 0"))
+    elif function == "screen_on":
+        #run_thread_delayed(lambda: screen_on())
+        screen_on()
+    elif function == "screen_off":
+        run_thread_delayed(lambda: ctypes.windll.user32.SendMessageW(65535, 274, 61808, 2))
     else:
         return "Unknown Systemfunction: " + function
     return "Systemfunction executing"
--- version.py
+++ version.py
@@ -2,7 +2,7 @@ import pyinstaller_versionfile
 
 pyinstaller_versionfile.create_versionfile(
     output_file="versionfile.txt",
-    version="1.6",
+    version="1.7",
     product_name="Message-Client",
     file_description="Message-Client",
     legal_copyright="GPL-3.0 license Copyright (c) 2024 Felix felixcrafter54@gmail.com",
